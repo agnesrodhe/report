c6eeec2813d0ca84a32990a0cb7a206d33c1753d
Agnes <agnes.rodhe@gmail.com>
2022-04-11 08:22:59 +0200

    Card and Deck file added

---
+++ src/Controller/CardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    /**
+     * @Route("/card", name="card")
+     */
+    public function card(): Response
+    {
+        return $this->render('card/card.html.twig');
+    }
+
+    /**
+     * @Route("/card/deck", name="showDeck")
+     */
+    public function showDeck(): Response
+    {
+        return $this->render('card/deck.html.twig');
+    }
+
+}
