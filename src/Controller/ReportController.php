ac3b004e471317d13060f51b5cfc632f200840dc
Agnes <agnes.rodhe@gmail.com>
2022-05-09 15:20:52 +0200

    Kmom06 metrics-page added

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -33,6 +33,14 @@ class ReportController extends AbstractController
         return $this->render('report.html.twig');
     }
 
+    /**
+     * @Route("/metrics", name="metrics")
+     */
+    public function metrics(): Response
+    {
+        return $this->render('metrics.html.twig');
+    }
+
     // /**
     //  * @Route("/api/lucky/number, name="lucky-number")
     //  */
