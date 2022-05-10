2a5064e744ab5b3c85ce7c5e213bdea34cf26067
Agnes <agnes.rodhe@gmail.com>
2022-05-10 10:05:16 +0200

    Cleaning in GameController

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -3,13 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Card\Game;
-use App\Card\Player;
-use App\Card\Deck;
 
 class GameController extends AbstractController
 {
